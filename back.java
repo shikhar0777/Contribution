7df1d263bacc3a3d183c2fe43a37ed133664cf56
Shikhar Pandey <smart.shikhar07@gmail.com>
2026-01-09 19:27:39 -0500

    Add basic Hello World program in back.java

--- back.java
+++ back.java
@@ -1,1 +1,7 @@
+// Example: A basic "Hello World" program
+public class Main { // Class declaration
+    public static void main(String[] args) { // Main method: entry point
+        System.out.println("Hello, World!"); // Statement (ends with a semicolon)
+    }
+}
 
