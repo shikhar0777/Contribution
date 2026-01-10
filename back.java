7eb3f68c7ab866af15aac0a8b651b204d6938959
Shikhar Pandey <smart.shikhar07@gmail.com>
2026-01-09 19:44:15 -0500

    Update back.java

--- back.java
+++ back.java
@@ -1,7 +1,7 @@
-// Example: A basic "Hello World" program
-public class Main { // Class declaration
-    public static void main(String[] args) { // Main method: entry point
-        System.out.println("Hello, World!"); // Statement (ends with a semicolon)
+
+public class Main { 
+    public static void main(String[] args) { 
+        System.out.println("Hello, World!"); 
     }
 }
 
