e1cba9653e728aa9d644537d6c88b1e631203d81
Shikhar Pandey <smart.shikhar07@gmail.com>
2026-01-10 21:48:39 -0500

    Correct main method syntax and print statement
    Fix syntax errors in main method declaration and output statement.

--- back.java
+++ back.java
@@ -7,7 +7,7 @@ public class Main {
 
 
 public class Main {
-    public static void main ( string[} args) {
+    public static void main ( string[ args) {
         systen.out.println("Introduction")
             }
 }
