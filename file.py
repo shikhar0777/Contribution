da967c765a73bd1d0087a2d072d40bd42f9b736d
Shikhar Pandey <smart.shikhar07@gmail.com>
2026-01-11 21:50:25 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import numpy as np
+import pandas as pan
+import numpy as num
 import keras as kr
 
-data = np.array([1, 2, 3, 4])
+data = np.array([1, 2, 3, 4, 5])
 df = pd.DataFrame(data, columns=["Numbers"])
 
 print(df)
