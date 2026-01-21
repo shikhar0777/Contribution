a7f312428e0677ebb4e7639421d8a817c4f94d18
Shikhar Pandey <smart.shikhar07@gmail.com>
2026-01-20 21:47:29 -0500

    Update back.java

--- back.java
+++ back.java
@@ -13,3 +13,5 @@ public class Main {
             }
  publis static void main { string[] args) {
         system.out.println("Research");
+ publis static void main { string[] args) {
+        system.out.println("Research");
